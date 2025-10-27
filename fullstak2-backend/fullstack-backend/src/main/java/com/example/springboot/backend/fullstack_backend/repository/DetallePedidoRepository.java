17f05b1f7581bed27d7c516d44ee622847bf7b55
MartinCofre <mar.cofrem@duocuc.cl>
2025-10-27 03:18:46 -0300

    Remove backend from Martin — keep only frontend

--- fullstak2-backend/fullstack-backend/src/main/java/com/example/springboot/backend/fullstack_backend/repository/DetallePedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.backend.fullstack_backend.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.springboot.backend.fullstack_backend.entity.DetallePedido;
-
-public interface DetallePedidoRepository extends CrudRepository<DetallePedido, Long> {
-
-}
